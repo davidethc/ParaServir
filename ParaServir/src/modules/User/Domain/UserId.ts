32bf6d6b9742b2a0044d5196910eaad9a206a434
Davide Tandazo <davidetandazo@MacBook-Air-de-Davide.local>
2025-11-17 20:01:12 -0800

    creando usuariio

---
+++ ParaServir/src/modules/User/Domain/UserId.ts
@@ -0,0 +1,14 @@
+export class UserId {
+    value : string;
+
+    constructor(value : string) {
+        this.value = value;
+        this.ensureIsValidId();
+    }
+
+    private ensureIsValidId() {
+        if (!this.value) {
+            throw new Error("Id is required");
+        }
+    }
+}
