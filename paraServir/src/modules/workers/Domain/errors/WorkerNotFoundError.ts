5d3e894483cd6e62ce0d866efc8cf6f72d74f550
Davide Tandazo <davidetandazo@MacBook-Air-de-Davide.local>
2025-12-08 08:28:29 -0800

    solo back

--- paraServir/src/modules/workers/Domain/errors/WorkerNotFoundError.ts
+++
@@ -1,7 +0,0 @@
-export class WorkerNotFoundError extends Error {
-    constructor(message: string = "Worker not found") {
-        super(message);
-        this.name = "WorkerNotFoundError";
-    }
-}
-
